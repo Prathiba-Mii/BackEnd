ea11c65785f195082cf1c314d98013265a688a4a
prathibhakamle@gmail.com <prathibhakamle@gmail.com>
2025-06-30 13:15:00 +0530

    nodejs

---
+++ Nodejs - 01/math.js
@@ -0,0 +1,18 @@
+export const sum = (a,b) => a+b;
+// exports const sum = (a,b) => a+b;
+export const mul = (a,b) => a*b;
+export const g = 9.8;
+export const PI = 3.14;
+
+// module.exports = "hello";
+
+// exports = 5;
+
+// module.exports = {
+//   sum:sum,
+//   mul: mul,
+//   g: g,
+//   PI: PI
+// };
+
+// module.exports = obj; 
---
+++ Nodejs - 01/script.js
@@ -0,0 +1,7 @@
+import {sum, PI} from "./math.js";
+import { generate } from "random-words";
+
+// console.log(sum(1,2));
+// console.log(PI);
+
+console.log(generate());
