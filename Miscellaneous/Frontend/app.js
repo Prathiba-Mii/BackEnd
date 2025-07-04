9fc9980445e25318569c8cbc5e9303508568dfff
prathibhakamle@gmail.com <prathibhakamle@gmail.com>
2025-07-04 11:33:45 +0530

    Moved backend files into Miscellaneous folder for better structure

---
+++ Miscellaneous/Frontend/app.js
@@ -0,0 +1,30 @@
+class Mammal { //base class /parent class
+  constructor(name) {
+    this.name = name;
+    this.type = "warm-blooded";
+  }
+
+  eat() {
+    console.log("I am eating");
+  }
+}
+
+class Dog extends Mammal { // child
+  constructor(name){
+    super(name);
+  }
+
+  bark() {
+    console.log("wooff..");
+  }
+}
+
+class Cat extends Mammal { //child
+  constructor(name){
+    super(name);
+  }
+
+  meow() {
+    console.log("meow..");
+  }
+}
